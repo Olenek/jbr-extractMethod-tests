6114d9fbbc286510ae2627af497bef72089182b0
Anton Ivanov <vultuc@gmail.com>
2021-07-09 12:03:15 +0300

    Create Dummy.java

---
+++ Dummy.java
@@ -0,0 +1,7 @@
+public class Dummy
+    public static void main(String[] args) throws Exception {
+         for(int i = 0; i < 100; i++){
+              System.out.println(++i);
+         }
+    }
+}
